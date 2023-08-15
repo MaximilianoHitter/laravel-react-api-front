c9f7a03641a1069e125d48b20717245db433530a
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2023-08-15 16:20:06 -0300

    movimiento en elementos

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className="bg-slate-100 min-h-screen">
       <nav className="rounder bg-black text-white px-2 py-2.5 sm:px-4 w-full mx-auto">
-        <div className="container mx-auto flex flex-wrap items-center justify-between hidden max-w-6xl mx-auto md:block md:w-auto sm:block sm:w-auto">
-          <a href="https://github.com/MaximilianoHitter" className="flex flex-items">
+        <div className="container mx-auto flex flex-col items-center justify-between hidden max-w-6xl mx-auto md:block md:w-auto sm:block sm:w-auto">
+        <button href="https://github.com/MaximilianoHitter" className="block rounded py-2 pr-4 pl-3 text-white">
             Maximiliano Hitter
-          </a>
-          
+          </button>
             <ul
-          className="mt-4 flex flex-col rounded-lg p-4 md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium sm:flex-col sm:space-x-0 sm:text-sm sm:font-medium">
+          className="mt-4 flex flex-col flex-end rounded-lg p-4 md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium sm:flex-col sm:space-x-0 sm:text-sm sm:font-medium">
+            
             <li>
               <Link
               to="/"
@@ -51,7 +51,6 @@ function App() {
             </li>
             </>}
             </ul>
-          
         </div>
       </nav>
       <div className="max-w-7xl mx-auto mt-6">
